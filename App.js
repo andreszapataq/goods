f1f82777298e0e450103f3a3f7a28bb2d2ae46db
andreszapataq <andreszapataq@gmail.com>
2020-05-11 11:52:37 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import { v4 as uuidv4 } from 'uuid';
+
+const App = () => {
+  const [items, setItems] = useState([
+    {id: uuidv4(), text: 'Milk' },
+    {id: uuidv4(), text: 'Eggs' },
+    {id: uuidv4(), text: 'Bread' },
+    {id: uuidv4(), text: 'Juice' }
+  ]);
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <FlatList 
+      data={items} 
+      renderItem={({item}) => <ListItem item={item} />}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+export default App;
