2de0965ce1838c9bb4d219254ae1c55aff7d5bff
andreszapataq <andreszapataq@gmail.com>
2020-05-17 17:37:21 -0500

    First commit

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
-const ListItem = ({item}) => {
+const ListItem = ({item, deleteItem}) => {
   return (
     <TouchableOpacity style={styles.listItem}>
       <View style={styles.listItemView}>
         <Text style={styles.listItemText}>{item.text}</Text>
+        <Icon name='trash' size={20} color='#777' onPress={() => deleteItem(item.id)} />
       </View>
     </TouchableOpacity>
   );
@@ -20,6 +22,11 @@ const styles = StyleSheet.create({
     },
     listItemView: {
         flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    listItemText: {
+      fontSize: 18
     }
 });
 
